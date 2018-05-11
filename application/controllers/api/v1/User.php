6033864a057c37fd46a4fcd7bfe92c02a2e33294
Yaju <you@example.com>
2018-05-11 08:53:43 +0530

    login registration done but not tested

--- application/controllers/api/v1/User.php
+++ application/controllers/api/v1/User.php
@@ -6,26 +6,113 @@ class User extends REST_Controller {
 
 	public function __construct() {
 		parent::__construct();
-		    $this->load->helper(array('url', 'form'));
-    		$this->load->model(array('Api_user_model'));
+		$this->load->helper(array('url', 'form'));
+		$this->load->model(array('Api_user_model'));
 	}
 
-	/* function name: registration_basic_post
-	*  purpose: gold registration
-	*  Author: Somwrita Debnath
-	*/
-   
-
-public function test11_post(){
-
-		//$country = $this->Api_user_model->getCountrylist();
 	
-		$response['status'] = true;
-		$response['response'] = array();
-		$response['message'] = "Data fetched successfully";
+
+	public function register_post(){
+
+		$response = array();
+
+		$first_name = $this->input->post('first_name');
+		$last_name = $this->input->post('last_name');
+		$email = $this->input->post('email');
+		$phone = $this->input->post('phone');
+		$gender = $this->input->post('gender');
+		$password = md5($this->input->post('password'));
+		$user_type = $this->input->post('user_type');
+
+		$otp = '0000';
+
+		if($this->Api_user_model->isEmailExist($email)){
+			$response['status'] = false;
+			$response['response'] = array();
+			$response['message'] = "email already exist";
+		}else{
+			$data = array()
+			$last_inserted_id = $this->Api_user_model->register($first_name,$last_name,$email,$phone,$gender,$password,$user_type,$otp);
+			$data['user_id'] = $last_inserted_id
+			$this->Api_user_model->sendOTP($phone,$otp);
+			$response['status'] = true;
+			$response['response'] = $data;
+			$response['message'] = "register successfully";
+
+		}
+
+		
 		
 		$this->response($response);
 
+
+	}
+
+	public function validate_otp_post(){
+		$response = array();
+
+		$user_id = $this->input->post('user_id');
+		$otp = $this->input->post('otp');
+		if($this->Api_user_model->validateOTP($user_id,$otp)){
+			$user_date = array();
+			$user_date = $this->Api_user_model->getUser($user_id)
+			if($user_date['status'] == '0'){
+				$response['status'] = false;
+				$response['response'] = array();
+				$response['message'] = "user status false";
+			}else if($user_date['is_active'] == '0'){
+				$response['status'] = false;
+				$response['response'] = array();
+				$response['message'] = $user_date['admin_message'];
+			}else if($user_date['is_phone_verified'] == '0'){
+				$response['status'] = true;
+				$response['response'] = array();
+				$response['message'] = "phone numver not verified";
+			}else{
+				$response['status'] = true;
+				$response['response'] = $user_date;
+				$response['message'] = "success";
+			}
+			
+		}else{
+			$response['status'] = false;
+			$response['response'] = array();
+			$response['message'] = "otp does not match";
+		}
+	}
+
+
+	public function login_post(){
+		$response = array();
+
+		$email = $this->input->post('email');
+		$password = md5($this->input->post('password'));
+
+		$user_id = $this->Api_user_model->isLoginValid($email,$password)
+		if($user_id == '0'){
+			$response['status'] = false;
+			$response['response'] = array();
+			$response['message'] = "email or passowrd does not match";
+		}else{
+			$user_date = $this->Api_user_model->getUser($user_id);
+			if($user_date['status'] == '0'){
+				$response['status'] = false;
+				$response['response'] = array();
+				$response['message'] = "user status false";
+			}else if($user_date['is_active'] == '0'){
+				$response['status'] = false;
+				$response['response'] = array();
+				$response['message'] = $user_date['admin_message'];
+			}else if($user_date['is_phone_verified'] == '0'){
+				$response['status'] = true;
+				$response['response'] = array();
+				$response['message'] = "phone numver not verified";
+			}else{
+				$response['status'] = true;
+				$response['response'] = $user_date;
+				$response['message'] = "login success";
+			}
+		}
 
 	}
 
--- application/models/Api_user_model.php
+++ application/models/Api_user_model.php
@@ -4,15 +4,72 @@ class Api_user_model extends CI_Model
 
 {
 
- 
 
-  public function getCountrylist(){
-     $rows = array();
-    
-    
-   
-  }
- 
-}
 
-?>
+	public function register($first_name, $last_name, $email, $phone,$gender, $password,$user_type,$otp){
+		$user = array();
+		$user['first_name'] = $first_name;
+		$user['last_name'] = $last_name;
+		$user['email'] = $email;
+		$user['phone'] = $phone;
+		$user['gender'] = $gender;
+		$user['password'] = $password;
+		$user['user_type'] = $user_type;
+		$user['otp'] = $otp;
+		$user['is_active'] = $user_type == 'CLIENT' ? '1' : '0';
+		$user['admin_message'] = $admin_message == 'CLIENT' ? '' : 'waiting for admin approval';
+		
+		$this->db->insert('users',$user);
+		$insert_id = $this->db->insert_id();
+		return $insert_id;
+
+
+	}
+
+	public function isEmailExist($email){
+
+		$rows = array();
+		$rows= $this->db->select('count(*) AS count')->where("email",$email)->get('users')->row_array();
+		return $rows['count']>0 ? true : false;
+	}
+
+	public function sendOTP($phone,$otp){
+			// OTP need to send Here
+	}
+
+	public function resendOTP($user_id,$otp){
+
+		$rows = array();
+		$rows= $this->db->select('phone')->where("id",$user_id)->get('users')->row_array();
+		if($rows['phone']>0){
+			$data  = array();
+			$data['otp']= $otp;
+			this->sendOTP($rows['phone'],$otp);
+			$this->db->where('id',$user_id)->update('users',$data);
+
+		}
+		return $rows['phone']>0 ? true : false;
+
+	}
+
+	public function validateOTP($user_id,$otp){
+
+		$rows = array();
+		$rows= $this->db->select('count(*) AS count')->where("id",$user_id)->where("otp",$otp)->get('users')->row_array();
+		if($rows['count']>0){
+			$data  = array();
+			$data['is_phone_verified']= '1';
+			$this->db->where('id',$user_id)->update('users',$data);
+
+		}
+		return $rows['count']>0 ? true : false;
+	}
+
+	public function getUser($userid){
+ 		$rows = array();
+     	$rows= $this->db->select('id,first_name, last_name, email,phone, image,is_phone_verified, is_active,admin_message, status')->where("id",$userid)>get('users')->row_array();
+     	return $rows;
+    
+	}
+
+	?>
