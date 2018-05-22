9c03e6f81afc1b4d3d65c9b4a340edcd623e356c
rajurana <rana.raju1511@gmail.com>
2018-05-23 00:48:09 +0530

    update client profile

--- application/controllers/api/v1/User.php
+++ application/controllers/api/v1/User.php
@@ -93,10 +93,10 @@ class User extends REST_Controller {
 	
 	public function imageCreate($profile_image, $type='client_profile_image') {
 		$file_name = uniqid();
-		if(!is_file('uploadImage')){
+		if(!is_dir('uploadImage')){
 			mkdir('uploadImage', '0777');
 		}
-		if(!is_file('uploadImage/'.$type)){
+		if(!is_dir('uploadImage/'.$type)){
 			mkdir('uploadImage/'.$type, '0777');
 		}
 		define('UPLOAD_DIR', './uploadImage/'.$type.'/');
