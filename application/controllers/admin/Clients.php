ab9e9a28d35f3ef718b234867c31cde99fe230cc
rajurana <rana.raju1511@gmail.com>
2018-05-31 00:14:20 +0530

    edit functionality added

--- application/controllers/admin/Clients.php
+++ application/controllers/admin/Clients.php
@@ -99,12 +99,14 @@ class Clients extends CI_Controller {
 	}
 
 
-	public function details() {
+	public function details($client_id) {
         $data = array();
         $data['title'] = 'Client Management';
-        $data['client_list'] = $this->admin_model->getClients();
+        $data['client_list'] = $this->admin_model->getClientDetails($client_id);
+        $data['all_case_list'] = $this->admin_model->getCaseList($client_id, 'ALL');
+        $data['open_case_list'] = $this->admin_model->getCaseList($client_id, 'PENDING');
 		/*echo "<pre />";
-		print_r($data['client_list']);exit;*/
+		print_r($data);exit;*/
 		$this->load->view('template/header.php', $data);
         $this->load->view('admin/client_details_view', $data);
 		$this->load->view('template/footer.php');
