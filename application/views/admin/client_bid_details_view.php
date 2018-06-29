2a938138e528d48f0f43fe84ad4706860878bfe1
Yaj <yaj@appsbee.com>
2018-06-29 19:50:14 +0530

    chosen

--- application/views/admin/client_bid_details_view.php
+++ application/views/admin/client_bid_details_view.php
@@ -263,16 +263,7 @@
 
 
 <script type="text/javascript">
-var config = {
-		    '.chosen-select'           : {max_selected_options: 200},
-		    '.chosen-select-deselect'  : {allow_single_deselect:true},
-		    '.chosen-select-no-single' : {disable_search_threshold:10},
-		    '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
-		    '.chosen-select-width'     : {width:"95%"}
-		  }
-		  for (var selector in config) {
-		    $(selector).chosen(config[selector]);
-		  }
+
 		  
 	var baseUrl = "<?=base_url()?>";
 	function disableUser(id){
